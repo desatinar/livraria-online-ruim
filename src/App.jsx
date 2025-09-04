ff954204b719e5c42e103bf0149bcaa72cbac047
Rodrigo Santos <rodrigosdn@gmail.com>
2025-09-03 23:35:08 -0300

    feat: estrutura básica das páginas e rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+const Home = () => <h1>Home Page - Encontre livros... se puder!</h1>;
+const Product = () => <h1>Product Page</h1>;
+const Cart = () => <h1>Cart</h1>;
+const Checkout = () => <h1>Checkout</h1>;
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <Router>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {/* Navegação inconsistente */}
+        <nav>
+          <Link to="/">Home</Link> |
+          <Link to="/product">Produto</Link> |
+          <Link to="/product">Produto (redundante)</Link> |
+          <Link to="/cart">Cart</Link> |
+          <Link to="/nao-existe">Link Quebrado</Link> |
+          <Link to="/checkout">Checkout</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="*" element={<h1>Erro - Página perdida. Boa sorte!</h1>} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
